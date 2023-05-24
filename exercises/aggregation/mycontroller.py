f88eeff3ce8396ad5582b61c9f97771b41c55a2a
Hudi <hudi1989@gmail.com>
2023-05-24 12:09:27 +0200

    Add port override and move out basic.p4

--- exercises/aggregation/mycontroller.py
+++ exercises/aggregation/mycontroller.py
@@ -57,16 +57,18 @@ def printCounter(p4info_helper, sw, counter_name, index):
             ))
 
 class SwitchConnection():
-    def __init__(self, device_id, filename):
+    def __init__(self, device_id, filename, port=None):
         self.device_id = device_id
         self.filename = filename
         self.p4info_path = f'./build/{self.filename}.p4.p4info.txt'
         self.bmv2_file_path = f'./build/{self.filename}.json'
         self.p4info_helper = p4runtime_lib.helper.P4InfoHelper(self.p4info_path)
 
+        self.port = f'5005{device_id+1}' if port is None else port
+
         self.connection = p4runtime_lib.bmv2.Bmv2SwitchConnection(
             name=f's{device_id+1}',
-            address=f'127.0.0.1:5005{device_id+1}',
+            address=f'127.0.0.1:{self.port}',
             device_id=device_id,
             proto_dump_file=f'logs/s{device_id+1}-p4runtime-requests.txt')
 
