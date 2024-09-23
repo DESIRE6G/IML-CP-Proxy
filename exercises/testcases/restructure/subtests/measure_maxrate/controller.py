981570c8b5f3f757e741b69eb63c3e756fe94880
Hudi <hudi1989@gmail.com>
2024-09-23 17:46:52 +0200

    Add measure testcase first iteration for testing P4Runtime rate

---
+++ exercises/testcases/restructure/subtests/measure_maxrate/controller.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python3
+import multiprocessing
+import queue
+import time
+from concurrent import futures
+from dataclasses import dataclass
+from typing import List
+
+import grpc
+from p4.v1.p4runtime_pb2 import SetForwardingPipelineConfigResponse, ReadResponse, WriteResponse
+
+from common.controller_helper import ControllerExceptionHandling
+from common.high_level_switch_connection import HighLevelSwitchConnection
+from p4.v1 import p4runtime_pb2
+from p4.v1.p4runtime_pb2_grpc import P4RuntimeServicer, add_P4RuntimeServicer_to_server
+
+
+class TickCounter:
+    def __init__(self) -> None:
+        self.counter = 0
+        self.last_reset = time.time()
+        self.ticks_per_sec_list = []
+
+    def tick(self) -> bool:
+        self.counter += 1
+        now_time = time.time()
+        if now_time - self.last_reset > 1:
+            self.ticks_per_sec_list.append(self.counter)
+            self.counter = 0
+            self.last_reset = now_time
+            return True
+
+        return False
+
+
+def find_center_average(l: list) -> float:
+    if len(l) == 1:
+        return l[0]
+    if len(l) == 2:
+        return (l[0] + l[1])/2
+    if len(l) == 3:
+        return sorted(l)[1]
+
+    sorted_list = sorted(l)
+    start = len(l) // 4
+    end = len(l) // 4 * 3
+
+    return sum(sorted_list[start:end]) / (end - start)
+
+
+class ProxyP4RuntimeServicer(P4RuntimeServicer):
+    def __init__(self, to_controller_queue: multiprocessing.Queue) -> None:
+        self.write_counter = 0
+        self.tick_counter = TickCounter()
+        self.to_controller_queue = to_controller_queue
+
+    def Write(self, request, context) -> None:
+        if self.tick_counter.tick():
+            print(self.tick_counter.ticks_per_sec_list, find_center_average(self.tick_counter.ticks_per_sec_list))
+            self.to_controller_queue.put(self.tick_counter.ticks_per_sec_list)
+
+        return WriteResponse()
+
+    def Read(self, original_request: p4runtime_pb2.ReadRequest, context):
+        result = ReadResponse()
+        yield result
+
+    def SetForwardingPipelineConfig(self, request: p4runtime_pb2.SetForwardingPipelineConfigRequest, context):
+        return SetForwardingPipelineConfigResponse()
+
+    def GetForwardingPipelineConfig(self, request: p4runtime_pb2.GetForwardingPipelineConfigRequest, context):
+        response = p4runtime_pb2.GetForwardingPipelineConfigResponse()
+        return response
+
+
+    def StreamChannel(self, request_iterator, context):
+        for request in request_iterator:
+            which_one = request.WhichOneof('update')
+            if which_one == 'arbitration':
+                response = p4runtime_pb2.StreamMessageResponse()
+                response.arbitration.device_id = request.arbitration.device_id
+                response.arbitration.election_id.high = 0
+                response.arbitration.election_id.low = 1
+                yield response
+            else:
+                raise Exception(f'Unhandled Stream field type {request.WhichOneof}')
+
+    def Capabilities(self, request: p4runtime_pb2.CapabilitiesRequest, context):
+        pass
+
+
+def start_dataplane_simulator(port: int, to_controller_queue: multiprocessing.Queue, stop_event: multiprocessing.Event) -> None:
+    servicer = ProxyP4RuntimeServicer(to_controller_queue)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    add_P4RuntimeServicer_to_server(servicer, server)
+    server.add_insecure_port(f'[::]:{port}')
+    server.start()
+
+    print(f'Opened GRPC: [::]:{port}')
+
+    try:
+        while not stop_event.is_set():
+            time.sleep(0.5)
+    except KeyboardInterrupt:
+        server.stop(grace=True)
+
+
+@dataclass
+class DataplaneInfoObject:
+    to_controller_queue: multiprocessing.Queue
+    process: multiprocessing.Process
+    stop_event : multiprocessing.Event
+
+
+with ControllerExceptionHandling():
+    dataplanes: List[DataplaneInfoObject] = []
+
+    for i in range(1):
+        to_controller_queue = multiprocessing.Queue()
+        stop_event = multiprocessing.Event()
+        process = multiprocessing.Process(target=start_dataplane_simulator, args=(50051 + i, to_controller_queue, stop_event, ))
+        dataplanes.append(
+            DataplaneInfoObject(
+                to_controller_queue=to_controller_queue,
+                process=process,
+                stop_event=stop_event
+            )
+        )
+        process.start()
+
+    while True:
+        time.sleep(1)
+
+    '''
+    try:
+        is_first = True
+        while True:
+            ticks = dataplanes[0].to_controller_queue.get(block=is_first)
+            print(ticks)
+            is_first = False
+    except queue.Empty:
+        pass
+    try:
+        while True:
+            pass
+    except KeyboardInterrupt:
+        for dataplane in dataplanes:
+            dataplane.stop_event.set()
+        for dataplane in dataplanes:
+            dataplane.process.join()
+    '''
---
+++ exercises/testcases/restructure/subtests/measure_maxrate/validator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import sys
+import time
+
+from common.high_level_switch_connection import HighLevelSwitchConnection
+from common.p4runtime_lib.switch import ShutdownAllSwitchConnections
+from common.redis_helper import compare_redis, wait_heartbeats_in_redis
+
+if __name__ == '__main__':
+    s1 = HighLevelSwitchConnection(0, 'part1', '60051')
+    table_entry = s1.p4info_helper.buildTableEntry(
+        table_name="MyIngress.state_setter",
+        match_fields={
+            'hdr.ethernet.dstAddr': '08:00:00:00:02:22'
+        },
+        action_name="MyIngress.state_set",
+        action_params={
+            "newState":66
+        })
+    for i in range(2000 * 5):
+        s1.connection.WriteTableEntry(table_entry)
