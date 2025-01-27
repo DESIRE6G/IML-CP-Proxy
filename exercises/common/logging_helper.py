ac0cc3687794d995010d841cec9c0f37fc2793f5
Hudi <hudi1989@gmail.com>
2025-01-27 16:26:00 +0100

    Fix configure_logger_with_common_settings to create logs folder if not exists

--- exercises/common/logging_helper.py
+++ exercises/common/logging_helper.py
@@ -1,7 +1,9 @@
 import logging
+import os
 
 
 def configure_logger_with_common_settings(log_filename: str) -> None:
+    os.makedirs('logs', exist_ok=True)
     logging.basicConfig(
         level=logging.DEBUG,
         format="%(asctime)s [%(levelname)s] %(message)s",
