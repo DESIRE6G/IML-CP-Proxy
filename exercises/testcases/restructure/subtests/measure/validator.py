71ae64af92c8604e21cae8ff8b4df0e9b8f76e5c
Hudi <hudi1989@gmail.com>
2025-02-22 23:51:39 +0100

    fix restructure/measure validator because of the improved controller

--- exercises/testcases/restructure/subtests/measure/validator.py
+++ exercises/testcases/restructure/subtests/measure/validator.py
@@ -9,9 +9,9 @@ from common.controller_helper import get_now_ts_us_int32
 from common.high_level_switch_connection import HighLevelSwitchConnection
 
 
-def sender_process(index: int, target_port: int, batch_size: int, rate_limit: int = None):
+def sender_process(index: int, program_name: str, prefix: str, target_port: int, batch_size: int, rate_limit: int = None):
     print(f'sender_process started {index=} {target_port=} {batch_size=} {rate_limit=} part{index+1}')
-    s = HighLevelSwitchConnection(index, f'part{index+1}', target_port, rate_limit=rate_limit)
+    s = HighLevelSwitchConnection(index, program_name, target_port, rate_limit=rate_limit)
     while True:
         request = p4runtime_pb2.WriteRequest()
         request.device_id = 0
@@ -19,11 +19,11 @@ def sender_process(index: int, target_port: int, batch_size: int, rate_limit: in
         ts = get_now_ts_us_int32()
         for _ in range(batch_size):
             table_entry = s.p4info_helper.buildTableEntry(
-            table_name="MyIngress.state_setter",
+            table_name=f"MyIngress.{prefix}state_setter",
                 match_fields={
                     'hdr.ethernet.dstAddr': '08:00:00:00:02:22'
                 },
-                action_name="MyIngress.state_set",
+                action_name=f"MyIngress.{prefix}state_set",
                 action_params={
                     "newState": ts
                 }
@@ -47,8 +47,14 @@ if __name__ == '__main__':
             rate_limit = args.dominant_sender_rate_limit
         else:
             rate_limit = args.rate_limit
+        if int(args.target_port) < 60000:
+            program_name = f'aggregated1234'
+            prefix = f'part{i+1}_'
+        else:
+            program_name = f'part{i+1}'
+            prefix = ''
 
-        process = multiprocessing.Process(target=sender_process, args=(i, int(args.target_port) + i, args.batch_size, rate_limit), daemon=True)
+        process = multiprocessing.Process(target=sender_process, args=(i, program_name, prefix, int(args.target_port) + i, args.batch_size, rate_limit), daemon=True)
         process.start()
 
     try:
