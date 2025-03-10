ffab8409bcb71fcd1c60fe6f812a8a94fad130da
Hudi <hudi1989@gmail.com>
2025-03-10 21:41:46 +0100

    Update drawer

--- exercises/drawer.py
+++ exercises/drawer.py
@@ -11,10 +11,12 @@ import numpy as np
 
 os.makedirs('images', exist_ok=True)
 
-#targets = ['fake_proxy']
+#targets = ['batch_delay_test', 'batch_delay_test_focused', 'unbalanced_flow', 'unbalanced_flow_delay']
 #targets = ['sending_rate_changing', 'fake_proxy', 'batch_size_changing', 'batch_delay_test', 'batch_delay_test_focused']
-targets = ['batch_delay_test', 'batch_delay_test_focused']
+#targets = ['batch_delay_test', 'batch_delay_test_focused']
+targets = ['sending_rate_changing']
 #targets = ['unbalanced_flow', 'unbalanced_flow_delay']
+#targets = ['multi_sender']
 #source_folder = '/home/hudi/remote-mounts/mininet/tutorials/exercises/results'
 source_folder = '/home/hudi/remote-mounts/elte-switch/exercises/results'
 target_folder = '/home/hudi/t4/proxy_doc/images'
@@ -37,6 +39,7 @@ for target in targets:
     force_xticks = None
     logx = False
     logy = False
+    force_order_for_values = {'mode': ['Real proxy', 'Fake proxy', 'Without proxy']}
 
 
     def load_and_prepare_df(filename):
@@ -52,22 +55,30 @@ for target in targets:
 
     if target == 'sending_rate_changing':
         df_original = load_and_prepare_df(f'{source_folder}/sending_rate_changing.csv')
-        df_original['target_port'] = df_original['target_port'].map({50051: 'Without proxy', 60051: 'With proxy'})
-        line_fields = ['target_port']
+        df_original['modep2'] = df_original['fake_proxy'].map({True: 10, False: 20})
+        df_original['modep1'] = df_original['target_port'].map({50051: 1, 60051: 2})
+        df_original['modep1pp2'] = df_original['modep1'] + df_original['modep2']
+
+        df_original['mode'] = df_original['modep1pp2'].map({12: 'Fake proxy', 21: 'Without proxy', 22: 'Real proxy'})
+
+        line_fields = ['mode']
         x_label = 'sending_rate'
         force_title = 'Request per second arrived to the dataplane'
         force_xlabel_legend = 'Request per second sent by control plane'
-    elif target == 'fake_proxy':
-        df_original = load_and_prepare_df(f'{source_folder}/fake_proxy.csv')
-        df_original['fake_proxy'] = df_original['fake_proxy'].map({True: 'Fake proxy', False: 'Real proxy'})
-        line_fields = ['fake_proxy']
+    elif target == 'sending_rate_changing_multi_sender':
+        df_original = load_and_prepare_df(f'{source_folder}/sending_rate_changing_multi_sender.csv')
+        line_fields = ['sender_num']
+        value_field_array = ['message_per_sec_mean']
         x_label = 'sending_rate'
         force_title = 'Request per second arrived to the dataplane'
         force_xlabel_legend = 'Request per second sent by control plane'
+
     elif target == 'batch_size_changing':
         df_original = load_and_prepare_df(f'{source_folder}/batch_size_changing.csv')
-        df_original['target_port'] = df_original['target_port'].map({50051: 'Without proxy', 60051: 'With proxy'})
+        df_original['mode'] = df_original['mode'].map({'without_proxy': 'Without proxy','fake_proxy': 'Fake proxy', 'real_proxy': 'Real proxy'})
+
         df_original = df_original[df_original['batch_size'] < 4096]
+        line_fields = ['mode']
         logx = True
         force_title = 'Table update per second arrived to the dataplane'
         force_xlabel_legend = 'Number of updates in one request'
@@ -79,12 +90,32 @@ for target in targets:
         logx = True
         force_title = 'Table update per second arrived to the dataplane'
         force_xlabel_legend = 'Max size of a batch in seconds'
+    elif target == 'batch_delay_test_delay':
+        df_original = load_and_prepare_df(f'{source_folder}/batch_delay_test.csv')
+        df_original['batch_delay'] = df_original['batch_delay'].fillna(0)
+        x_label = 'batch_delay'
+        line_fields = ['sender_num']
+        value_field_array = ['delay_average']
+        logx = True
+        logy = True
+        force_title = 'Table update per second arrived to the dataplane'
+        force_xlabel_legend = 'Max size of a batch in seconds'
     elif target == 'batch_delay_test_focused':
         df_original = load_and_prepare_df(f'{source_folder}/batch_delay_test.csv')
         df_original['batch_delay'] = df_original['batch_delay'].fillna(0)
         df_original = df_original[df_original['batch_delay'] < 0.0033]
         x_label = 'batch_delay'
         line_fields = ['sender_num']
+        force_title = 'Table update per second arrived to the dataplane'
+        force_xlabel_legend = 'Max size of a batch in seconds'
+        logx = False
+    elif target == 'batch_delay_test_focused_delay':
+        df_original = load_and_prepare_df(f'{source_folder}/batch_delay_test.csv')
+        df_original['batch_delay'] = df_original['batch_delay'].fillna(0)
+        df_original = df_original[df_original['batch_delay'] < 0.0033]
+        x_label = 'batch_delay'
+        line_fields = ['sender_num']
+        value_field_array = ['delay_average']
         force_title = 'Table update per second arrived to the dataplane'
         force_xlabel_legend = 'Max size of a batch in seconds'
         logx = False
@@ -102,9 +133,18 @@ for target in targets:
         merge_value_field_plots = True
         force_title = 'Table update delays per tenant'
         x_label = 'dominant_sender_rate_limit'
+    elif target == 'multi_sender':
+        df_original = load_and_prepare_df(f'{source_folder}/multi_sender.csv')
+        df_original = df_original[df_original['sender_num'] == 4]
+        line_fields = []
+        value_field_array = ['average_by_table.part1', 'average_by_table.part2', 'average_by_table.part3', 'average_by_table.part4']
+        x_label = 'rate_limit'
+        grid_y_field = 'sender_num'
+        merge_value_field_plots = True
+        force_title = 'Table updates per tenant'
     else:
         raise Exception(f'Unknowon target "{target}"')
-    print(df_original)
+    #print(df_original)
     output_filename = str(target) + '.png'
 
     # column | row | line
@@ -114,12 +154,26 @@ for target in targets:
 
 
     style_rules = {
+        'mode': {'Fake proxy': 'b-', 'Without proxy': 'r', 'Real proxy': 'g'},
     }
-
     regexp_style_rules = {
     }
 
-    unique_values_dict = {c: sorted(df_original[c].unique()) for c in df_original.columns}
+    unique_values_dict = {}
+
+    for c in df_original.columns:
+        if c in force_order_for_values:
+            print('HELLO')
+
+            vals_unique_sorted = sorted(df_original[c].unique())
+            local_vals = [v for v in force_order_for_values[c] if v in vals_unique_sorted]
+
+            for v in vals_unique_sorted:
+                if v not in local_vals:
+                    local_vals.append(v)
+            unique_values_dict[c] = local_vals
+        else:
+            unique_values_dict[c] = sorted(df_original[c].unique())
 
     value_field_array_caused_size_need = 1 if merge_value_field_plots else len(value_field_array)
     col_multiplier = value_field_array_caused_size_need if multi_y_value_mode == MULTI_Y_VALUE_MODE_COLUMN else 1
@@ -193,7 +247,6 @@ for target in targets:
                     final_line_fields += [value_field]
 
                 line_field_conditions_array = list(itertools.product(*line_fields_values))
-
                 for line_field_conditions in line_field_conditions_array:
                     print("-------", final_line_fields, line_field_conditions)
 
@@ -249,7 +302,13 @@ for target in targets:
                     else:
                         fig_x_size = (6 if small else 16) * col_num
                         fig_y_size = (3.5 if small else 7) * row_num
-                    final_title = force_title if force_title is not None else title
+                    if force_title is None:
+                        final_title = title
+                    else:
+                        if x_position == 0 and y_position == 0:
+                            final_title = force_title
+                        else:
+                            final_title = ''
 
                     if plot_type == 'bar':
                         ax = draw_df.plot.bar(x=x_label, ax=ax, legend=has_legend, figsize=(fig_x_size, fig_y_size),
