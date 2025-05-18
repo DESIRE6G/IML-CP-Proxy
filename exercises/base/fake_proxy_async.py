a59ae15fb9bf5629777e5a77918a2a9017ca5590
Hudi <hudi1989@gmail.com>
2025-05-18 12:12:32 +0200

    Use asyncio in fake proxy async instead of multiprocess

--- exercises/base/fake_proxy_async.py
+++ exercises/base/fake_proxy_async.py
@@ -28,7 +28,13 @@ class ProxyP4RuntimeServicer(P4RuntimeServicer):
         self.target_client_stub = target_client_stub
 
     async def Write(self, request, context) -> None:
-        asyncio.ensure_future(self.target_client_stub.Write(request))
+        new_request = p4runtime_pb2.WriteRequest()
+        new_request.device_id = 0
+        new_request.election_id.low = 1
+        for update in request.updates:
+            update_in_request = new_request.updates.add()
+            update_in_request.CopyFrom(update)
+        asyncio.ensure_future(self.target_client_stub.Write(new_request))
         return p4runtime_pb2.WriteResponse()
 
     async def Read(self, original_request: p4runtime_pb2.ReadRequest, context):
@@ -56,8 +62,6 @@ class ProxyP4RuntimeServicer(P4RuntimeServicer):
             print('StreamChannel')
 
             async for request in request_iterator:
-                print('ARRIVED')
-                print(request)
                 which_one = request.WhichOneof('update')
                 if which_one == 'arbitration':
                     response = p4runtime_pb2.StreamMessageResponse()
@@ -75,82 +79,38 @@ class ProxyP4RuntimeServicer(P4RuntimeServicer):
         pass
 
 
+async def dataplane_simulator_async_main(index: int) -> asyncio.Task:
+    port = 60051 + index
+    target_port = 50051 + index
+    target_address = f'127.0.0.1:{target_port}'
 
+    channel = grpc.aio.insecure_channel(target_address)
+    target_client_stub = p4runtime_pb2_grpc.P4RuntimeStub(channel)
 
+    servicer = ProxyP4RuntimeServicer(target_client_stub)
+    server = grpc.aio.server()
+    add_P4RuntimeServicer_to_server(servicer, server)
+    server.add_insecure_port(f'[::]:{port}')
+    await server.start()
+    print(f'Opened GRPC: [::]:{port}')
 
-def start_dataplane_simulator(index: int, queue: multiprocessing.Queue, stop_event: multiprocessing.Event) -> None:
-    async def dataplane_simulator_async_main():
-        port = 60051 + index
-        target_port = 50051 + index
-        target_address = f'127.0.0.1:{target_port}'
-
-        channel = grpc.aio.insecure_channel(target_address)
-        target_client_stub = p4runtime_pb2_grpc.P4RuntimeStub(channel)
-        #
-        # requests_stream = IterableQueue()
-        # stream_msg_resp = target_client_stub.StreamChannel(iter(requests_stream))
-        #
-        # request = p4runtime_pb2.StreamMessageRequest()
-        # request.arbitration.device_id = 0
-        # request.arbitration.election_id.high = 0
-        # request.arbitration.election_id.low = 1
-        #
-        # requests_stream.put(request)
-        # print(request)
-
-        servicer = ProxyP4RuntimeServicer(target_client_stub)
-        server = grpc.aio.server()
-        add_P4RuntimeServicer_to_server(servicer, server)
-        server.add_insecure_port(f'[::]:{port}')
-        await server.start()
-
-        print(f'Opened GRPC: [::]:{port}')
-        queue.put('ready')
+    async def terminate_async():
         try:
             await server.wait_for_termination()
         finally:
             await server.stop(10)
 
-    asyncio.run(dataplane_simulator_async_main())
-
-
-
-@dataclass
-class DataplaneInfoObject:
-    process: multiprocessing.Process
-    queue: multiprocessing.Queue
-    stop_event : multiprocessing.Event
+    return asyncio.create_task(terminate_async())
 
 parser = argparse.ArgumentParser(prog='Fake Proxy')
 parser.add_argument('--proxy_size', default=2, type=int)
 args = parser.parse_args()
 
-
 with ControllerExceptionHandling():
-    dataplanes: List[DataplaneInfoObject] = []
+    async def async_main():
+        tasks = [await dataplane_simulator_async_main(i) for i in range(args.proxy_size)]
+        print('Proxy is ready')
 
-    for i in range(args.proxy_size):
-        stop_event = multiprocessing.Event()
-        queue = multiprocessing.Queue()
-        process = multiprocessing.Process(target=start_dataplane_simulator, args=(i, queue, stop_event, ))
-        dataplanes.append(
-            DataplaneInfoObject(
-                process=process,
-                queue=queue,
-                stop_event=stop_event
-            )
-        )
-        process.start()
+        await asyncio.gather(*tasks)
 
-    for i in range(args.proxy_size):
-        print(i, dataplanes[i].queue.get())
-    print('Proxy is ready')
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        for dataplane in dataplanes:
-            dataplane.stop_event.set()
-        for dataplane in dataplanes:
-            dataplane.process.join()
-
+    asyncio.run(async_main())
