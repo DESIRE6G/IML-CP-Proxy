fb2f646f9416639e73c8a5b1757a46f30bf947c2
Hudi <hudi1989@gmail.com>
2025-09-21 14:57:16 +0200

    Add example docker setup

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:3.8.10
+
+COPY ./requirements.txt /app/requirements.txt
+
+RUN pip install --no-cache-dir -r /app/requirements.txt
+
+WORKDIR /app
+
+EXPOSE 60051-60054
+
+ENTRYPOINT ["python", "proxy.py"]
---
+++ example/controller/controller.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+
+from common.controller_helper import ControllerExceptionHandling
+from common.high_level_switch_connection import HighLevelSwitchConnection
+from common.p4runtime_lib.convert import decodeIPv4
+
+with ControllerExceptionHandling():
+    s1 = HighLevelSwitchConnection(0, 'basicv3', '60051', host='p4runtime-proxy')
+    table_entry = s1.p4info_helper.buildTableEntry(
+        table_name="MyIngress.ipv4_lpm1",
+        match_fields={
+            "hdr.ipv4.dstAddr": ('10.0.2.20', 32)
+        },
+        action_name="MyIngress.chg_addr",
+        action_params={
+            'port': 2,
+            'dstAddr': '08:00:00:00:02:00',
+        }
+    )
+    s1.connection.WriteTableEntry(table_entry)
+
+
+    table_entry = s1.p4info_helper.buildTableEntry(
+        table_name="MyIngress.ipv4_lpm2",
+        match_fields={
+            "hdr.ipv4.dstAddr": ('10.0.2.20', 32)
+        },
+        action_name="MyIngress.set_port"
+    )
+    s1.connection.WriteTableEntry(table_entry)
+
+
+    table_entry = s1.p4info_helper.buildTableEntry(
+        table_name="MyIngress.just_another",
+        match_fields={
+            "hdr.ethernet.dstAddr": '08:00:00:00:02:23'
+        },
+        action_name="MyIngress.drop"
+    )
+    s1.connection.WriteTableEntry(table_entry)
+    
+    
+    for response in s1.connection.ReadTableEntries():
+        for entity in response.entities:
+            entry = entity.table_entry
+            try:
+               print('----------------')
+               print(f'Filled table entry in: {s1.p4info_helper.get_tables_name(entry.table_id)}')
+               print(entry)
+            except AttributeError as e:
+                print(e)
