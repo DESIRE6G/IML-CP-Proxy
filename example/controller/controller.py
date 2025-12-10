57e7e42fc8aafe574a9e02151a11daf6df4da333
Hudi <hudi1989@gmail.com>
2025-12-11 00:32:20 +0100

    Add demo integration test to CI

--- example/controller/controller.py
+++ example/controller/controller.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
-import sys
+import time
 
 from common.controller_helper import ControllerExceptionHandling
 from common.high_level_switch_connection import HighLevelSwitchConnection
-from common.p4runtime_lib.convert import decodeIPv4
+from google.protobuf.json_format import MessageToJson
 
 with ControllerExceptionHandling():
+    expected_entries_json = []
+
     s1 = HighLevelSwitchConnection(0, 'basicv3', '60051', host='p4runtime-proxy')
     table_entry = s1.p4info_helper.build_table_entry(
         table_name="MyIngress.ipv4_lpm1",
@@ -20,6 +21,7 @@ with ControllerExceptionHandling():
         }
     )
     s1.connection.WriteTableEntry(table_entry)
+    expected_entries_json.append(MessageToJson(table_entry))
 
 
     table_entry = s1.p4info_helper.build_table_entry(
@@ -30,6 +32,7 @@ with ControllerExceptionHandling():
         action_name="MyIngress.set_port"
     )
     s1.connection.WriteTableEntry(table_entry)
+    expected_entries_json.append(MessageToJson(table_entry))
 
 
     table_entry = s1.p4info_helper.build_table_entry(
@@ -40,14 +43,27 @@ with ControllerExceptionHandling():
         action_name="MyIngress.drop"
     )
     s1.connection.WriteTableEntry(table_entry)
+    expected_entries_json.append(MessageToJson(table_entry))
     
     
     for response in s1.connection.ReadTableEntries():
         for entity in response.entities:
             entry = entity.table_entry
+            entry_json = MessageToJson(entry)
+            expected_entries_json.remove(entry_json)
             try:
                print('----------------')
                print(f'Filled table entry in: {s1.p4info_helper.get_tables_name(entry.table_id)}')
                print(entry)
             except AttributeError as e:
                 print(e)
+
+    if len(expected_entries_json) > 0:
+        print(f"Missing {len(expected_entries_json)} entries", flush=True)
+        for expected_entry in expected_entries_json:
+            print(expected_entry)
+
+        raise Exception(f"Verification Failed: {len(expected_entries_json)} missing entries.")
+
+    print('Tables filled successfully', flush=True)
+    time.sleep(2)
