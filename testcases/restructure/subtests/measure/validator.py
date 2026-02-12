32cdcb5b9ff6e2522bde22c6c6bef23e09ecddd8
Hudi <hudi1989@gmail.com>
2026-02-12 23:56:02 +0100

    Use asyncio switch as sender in restructure measure

--- testcases/restructure/subtests/measure/validator.py
+++ testcases/restructure/subtests/measure/validator.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python3
 import argparse
-import multiprocessing
-import time
+import asyncio
 
 from p4.v1 import p4runtime_pb2
 
 from common.controller_helper import get_now_ts_us_int32
-from common.high_level_switch_connection import HighLevelSwitchConnection
+from common.high_level_switch_connection_async import HighLevelSwitchConnection
 
 
-def sender_process(index: int, program_name: str, prefix: str, target_port: int, batch_size: int, rate_limit: int = None):
-    print(f'sender_process started {index=} {program_name=} {prefix=} {target_port=} {batch_size=} {rate_limit=} part{index+1}')
-    s = HighLevelSwitchConnection(index, program_name, target_port, rate_limit=rate_limit)
+async def sender_task(index: int, program_name: str, prefix: str, target_port: int, batch_size: int, rate_limit: int = None):
+    print(f'sender_task started {index=} {program_name=} {prefix=} {target_port=} {batch_size=} {rate_limit=} part{index+1}')
+
+    s = HighLevelSwitchConnection(
+        device_id=index,
+        filename=program_name,
+        port=target_port,
+        send_p4info=True,
+        reset_dataplane=True,
+        rate_limit=rate_limit
+    )
+    await s.init()
+
     while True:
         request = p4runtime_pb2.WriteRequest()
         request.device_id = 0
@@ -19,7 +28,7 @@ def sender_process(index: int, program_name: str, prefix: str, target_port: int,
         ts = get_now_ts_us_int32()
         for _ in range(batch_size):
             table_entry = s.p4info_helper.build_table_entry(
-            table_name=f"MyIngress.{prefix}state_setter",
+                table_name=f"MyIngress.{prefix}state_setter",
                 match_fields={
                     'hdr.ethernet.dstAddr': '08:00:00:00:02:22'
                 },
@@ -31,34 +40,47 @@ def sender_process(index: int, program_name: str, prefix: str, target_port: int,
             update = request.updates.add()
             update.type = p4runtime_pb2.Update.INSERT
             update.entity.table_entry.CopyFrom(table_entry)
-        s.connection.client_stub.Write(request)
+        await s.connection.client_stub.Write(request)
 
-parser = argparse.ArgumentParser(prog='Validator')
-parser.add_argument('--batch_size', default=1, type=int)
-parser.add_argument('--sender_num', default=1, type=int)
-parser.add_argument('--rate_limit', default=None, type=int)
-parser.add_argument('--target_port', default='60051')
-parser.add_argument('--dominant_sender_rate_limit', default=None, type=int)
-args = parser.parse_args()
+        await asyncio.sleep(0)
 
-if __name__ == '__main__':
+
+async def main():
+    parser = argparse.ArgumentParser(prog='Validator')
+    parser.add_argument('--batch_size', default=1, type=int)
+    parser.add_argument('--sender_num', default=1, type=int)
+    parser.add_argument('--rate_limit', default=None, type=int)
+    parser.add_argument('--target_port', default='60051')
+    parser.add_argument('--dominant_sender_rate_limit', default=None, type=int)
+    args = parser.parse_args()
+
+    tasks = []
     for i in range(args.sender_num):
         if args.dominant_sender_rate_limit is not None and i == 0:
             rate_limit = args.dominant_sender_rate_limit
         else:
             rate_limit = args.rate_limit
+
         if int(args.target_port) < 60000:
-            program_name = f'aggregated1234'
+            program_name = 'aggregated1234'
             prefix = f'part{i+1}_'
         else:
             program_name = f'part{i+1}'
             prefix = ''
 
-        process = multiprocessing.Process(target=sender_process, args=(i, program_name, prefix, int(args.target_port) + i, args.batch_size, rate_limit), daemon=True)
-        process.start()
+        task = asyncio.create_task(
+            sender_task(i, program_name, prefix, int(args.target_port) + i, args.batch_size, rate_limit)
+        )
+        tasks.append(task)
 
     try:
-        while True:
-            time.sleep(1)
+        await asyncio.gather(*tasks)
+    except asyncio.CancelledError:
+        print('Tasks cancelled, stopping.')
+
+
+if __name__ == '__main__':
+    try:
+        asyncio.run(main())
     except KeyboardInterrupt:
         print('KeyboardInterrupt arrived, stopping.')
